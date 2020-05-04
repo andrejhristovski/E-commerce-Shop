7c5499a91ef29b72e96d4fe589b975b956de46d1
Andrej <andrejhristovski@yahoo.com>
2020-05-04 19:48:42 +0200

    first commit

---
+++ e-commerce-shop/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./homepage.component";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>asdasdas</h1>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
