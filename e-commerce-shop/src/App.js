049c3f3999431be2f07d2e7b2fb342a44fd29242
Andrej <andrejhristovski@yahoo.com>
2020-05-04 22:02:02 +0200

    menu-items

--- e-commerce-shop/src/App.js
+++ e-commerce-shop/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>asdasdas</h1>
       <HomePage />
     </div>
   );
