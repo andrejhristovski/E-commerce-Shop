bdc0fc77cd25ae2430525f601b6f801394e80113
Andrej <andrejhristovski@yahoo.com>
2020-05-06 01:33:21 +0200

    ShopPage

--- e-commerce-shop/src/App.js
+++ e-commerce-shop/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
