03ca3d3c62c3e6f42763da60a1a50322aa9b826f
Andrej Hristovski <andrejhristovski@yahoo.com>
2020-05-18 16:29:45 +0200

    header

--- e-commerce-shop/src/App.js
+++ e-commerce-shop/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
